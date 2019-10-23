0dc9e1cf316717efd4143c73a323bfeb6c5b8bf9
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2019-10-23 10:31:46 +0200

    update

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,22 +7,34 @@
             v-bind:key="card.id"
             v-bind:card="card"/>
         </div>
+        <p v-on:click.prevent="toggleForm()">+ Show card</p>
+        <NewCard v-if="showForm"></NewCard>
     </div>
 </template>
 
 <script>
 import Card from "./Card.vue"
+import NewCard from "./NewCard.vue"
 export default {
     name: "List",
     components: {
-        card: Card
+        card: Card,
+        NewCard
     },
     props: {
         list:Object
     },
+    data: () => {
+        showForm: true
+    },
     filters: {
         countDone: function(cards) {
             return cards.filter(card => card.status && card.status === "done").length;
+        }
+    },
+    methods: {
+        toggleForm: function(){
+            this.showForm = !this.showForm;
         }
     }
 }
---
+++ src/components/NewCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="grid-row">
+            <label for="title">Title :</label>
+            <input id="title"/>
+        </div>
+        <div class="grid-row">
+            <label for="description">Description :</label>
+            <input id="description"/>
+        </div>
+        <div class="grid-row">
+            <label for="velocity">Velocity :</label>
+            <input id="velocity"/>
+        </div>
+        <div class="grid-row">
+            <label>Status:</label>
+            <label>New <input type="radio" name="status" value="new"></label>
+            <label>Done <input type="radio" name="status" value="done"></label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NewCard"
+}
+</script>
+
+<style scoped>
+    .grid-row{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+</style>
