fff3f14d0fd57a18b78378773bd9a568672b23ec
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2019-11-13 11:48:24 +0100

    init form

---
+++ src/components/Field.vue
@@ -0,0 +1,53 @@
+<template>
+    <fieldset>
+        <label :for="id">{{label}}</label>
+        <select v-if="type == 'select'" :id="id" :name="name" :placeholder="placeholder">
+            <option
+                v-for="option in options"
+                :key="option.id"
+                :list="option"
+                :selected="option.value == value">{{ option.label }}</option>
+        </select>
+        <input 
+            v-if="type == 'checkbox' || type == 'radio'"
+            v-for="option in options"
+            :key="option.id"
+            :list="option"
+            :id="option.id"
+            :type="type"
+            :name="name"
+            :value="option.value"
+            :placeholder="option.label"
+            :checked="option.value == true"
+            />
+        <input 
+            v-if="type != 'select'"
+            :id="id"
+            :type="type"
+            :name="name"
+            :value="value"
+            :placeholder="value"
+            
+            />
+    </fieldset>
+</template>
+
+<script>
+export default {
+    name:"Field",
+    props:{
+        label: String,
+        name: String,
+        id: String,
+        type: String,
+        value: String,
+        placeholder: String,
+        options: Object
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Formik.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <h1>{{ name }}</h1>
+    <form
+      @submit.prevent="handleSubmit"
+      :action="action"
+    >
+      <Field
+        v-for="field in fields"
+        :key="field.id"
+        :list="field"
+        ></Field>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Formik",
+    props: {
+        fields: Object,
+        action: String
+    },
+    data: () => ({
+        
+    }),
+    methods: {
+        handleSubmit: () => {
+            this.$emit('formik-submit', { fields });
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
